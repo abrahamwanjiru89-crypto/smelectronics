59fc1693f3542716a3534ada77f30907a0d07384
abrahamwanjiru89-crypto <abrahamwanjiru89@gmail.com>
2026-06-12 19:16:19 +0300

    Update repair.js

--- shop/repair.js
+++ shop/repair.js
@@ -2,7 +2,7 @@
 // S.M DYNAMICS REPAIR PAGE JS - FULLY FUNCTIONAL
 // ============================================
 (function() {
-window.repairServices = window.repairServices | | [];
+window.repairServices = window.repairServices || [];
 let repairServices = [];
 let spareParts = [];
 let currentCategory = 'all';
